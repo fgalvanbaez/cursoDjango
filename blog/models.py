dc2ca3664f0992cceb160002a1a5ecad3ff184b1
Francisco A Galván Báez <francisco.galvan.baez@gmail.com>
2015-03-14 12:07:53 +0000

    Subo el proyecto del curso de Django

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Post (models.Model):
+    autor = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=255)
+    cuerpo = models.TextField()
